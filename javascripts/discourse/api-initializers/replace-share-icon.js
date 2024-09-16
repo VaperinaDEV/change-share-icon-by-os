4324a6ddc94aabe8492bb591ac1a2f1f0a0339f4
Don <71207900+VaperinaDEV@users.noreply.github.com>
2024-09-16 08:39:59 +0200

    Add option to disable post share icon on desktop

--- javascripts/discourse/api-initializers/replace-share-icon.js
+++ javascripts/discourse/api-initializers/replace-share-icon.js
@@ -7,14 +7,18 @@ export default {
     withPluginApi("0.8.7", (api) => {
       this.capabilities = container.lookup("capabilities:main");
       const isAndroid = this.capabilities.isAndroid;
+      const site = container.lookup("service:site");
+      const isDesktop = site.desktopView;
+      const disableIconChangeOnDesktop = settings.disable_icon_change_on_desktop;
 
       if (isAndroid) {
         api.replaceIcon("d-post-share", "share-nodes");
         api.replaceIcon("d-topic-share", "share-nodes");
-      } else {
-        api.replaceIcon("d-post-share", "arrow-up-from-bracket");
+      } else if (isDesktop && disableIconChangeOnDesktop) {
         api.replaceIcon("d-topic-share", "arrow-up-from-bracket");
-      }
+      } else {
+        api.replaceIcon("d-topic-share", "arrow-up-from-bracket");
+        api.replaceIcon("d-post-share", "arrow-up-from-bracket");
     });
   },
 };
